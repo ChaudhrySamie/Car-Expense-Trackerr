4601121e0bd75a5b0608d785606d7d8a9ac02aae
Chaudhry Samie Tahir <badshayahoopakistan@gmail.com>
2026-08-04 00:26:28 +0500

    Remove Firebase configuration from firebase.ts
    Removed sensitive Firebase configuration details.

--- services/firebase.ts
+++ services/firebase.ts
@@ -6,12 +6,7 @@ import 'firebase/compat/messaging';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYran1D1dEXlPd5Z7Ff-KUOsR9YmlYgDY",
-  authDomain: "car-expense-tracker-6b0b2.firebaseapp.com",
-  projectId: "car-expense-tracker-6b0b2",
-  storageBucket: "car-expense-tracker-6b0b2.firebasestorage.app",
-  messagingSenderId: "122623897969",
-  appId: "1:122623897969:web:122501e4a48f3c29c146d0",
+
 };
 
 // Initialize Firebase
